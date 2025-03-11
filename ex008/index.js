add166d3671c4b8c7caa44b4f367fddd0679d346
PedroPoliceno <pedrooswg@gmail.com>
2025-03-11 06:40:25 -0300

    aula 11 (condições pt1)

---
+++ ex008/index.js
@@ -0,0 +1,6 @@
+var vel = 71
+console.log(`A sua velocidade é ${vel}KM/h`)
+if(vel >= 60){
+    console.log('A sua velocidade ultrapassou o limite, você foi MULTADO!')
+}
+console.log('Use sempre cinto de segurança')
---
+++ ex009/index.js
@@ -0,0 +1,6 @@
+var país = 'Brasil'
+if(país == 'Brasil'){
+    console.log(`Vivendo no ${país}, você é Brasileiro!`)
+}else{
+    console.log(`Vivendo em ${país}, você é Estrangeiro!`)
+}
