501a14d5bb4c133e0c441864f3a6c56bafa21126
PedroPoliceno <pedrooswg@gmail.com>
2025-03-19 19:33:54 -0300

    terminado exercício de funções e vetores

--- ex022/script.js
+++ ex022/script.js
@@ -41,7 +41,29 @@ function fin(){
     }else{
         var tot = valores.length
 
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for (var pos in valores){
+            soma += valores[pos]
+
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+
+        media = soma / tot
+
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O menor valor é ${menor}</p>`
+        res.innerHTML += `<p>O maior valor é ${maior}</p>`
+        res.innerHTML += `<p>A soma te todos os valores é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+    
     }
 }
